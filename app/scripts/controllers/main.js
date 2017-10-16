b0b0c1f4120a8a6077173164b4e0e7b1bb93b75b
Rafael Carneiro de Castro <rafaelcarneiroget@hotmail.com>
2017-10-16 15:16:18 -0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apSimulatorWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apSimulatorWebApp
+ */
+angular.module('apSimulatorWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
